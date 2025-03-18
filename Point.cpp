d36549aa1beb38cfe9e2d3beb6ac20a5f0515f48
michaelchan132 <110430837+michaelchan132@users.noreply.github.com>
2025-03-18 03:43:05 +0000

    lab4

--- Point.cpp
+++ Point.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-#include <cmath> // For sqrt() and pow()
+#include <cmath> 
 #include "Point.h"
 
 using namespace std;
+
+Point::Point(double xCoord, double yCoord) : x(xCoord), y(yCoord) {}
+
+double Point::distanceTo(const Point& other) const {
+    return sqrt(pow(other.x - x, 2) + pow(other.y - y, 2));
+}
+
+void Point::print() const {
+    cout << "(" << x << ", " << y << ")" << endl;
+}
